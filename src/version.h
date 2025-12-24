a2b91488a891ee5e99e24c5459c7bbf0caa384b6
Apolo1061 <Apolonegocios@hotmail.com>
2025-12-23 23:22:28 -0500

    Fix date length for static assertion

--- src/version.h
+++ src/version.h
@@ -2,6 +2,6 @@
 #define UPX_VERSION_HEX      0x050100 /* 05.01.00 */
 #define UPX_VERSION_STRING   "5.1.0 [AvalonNet AVLN-PACK]"
 #define UPX_VERSION_STRING4  "5.10"
-#define UPX_VERSION_DATE     "Dec 2025"
+#define UPX_VERSION_DATE     "Dec 24th 2025"
 #define UPX_VERSION_DATE_ISO "2025-12-24"
 #define UPX_VERSION_YEAR     "2025"
